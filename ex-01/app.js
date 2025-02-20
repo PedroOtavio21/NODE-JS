d795edc309650f16ac9a3e338b5af1363c2f4927
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-02-20 10:57:23 -0300

    Deleta diretório de exercício 1 para revisão do módulo

--- ex-01/app.js
+++
@@ -1,7 +0,0 @@
-import { createFile, showFile, updateFile, deleteFile } from "./files.js";
-
-createFile("Conteúdo inicial do arquivo.\nCriado com o módulo fs do Node.js.");
-showFile()
-updateFile("Conteúdo modificado!")
-showFile()
-deleteFile()
